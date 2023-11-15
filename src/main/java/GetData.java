448c7623f5872e37564ca45e0a82b0d52e20e227
Beket <beket.bayemirov@datathings.com>
2023-11-15 15:55:49 +0100

    gradle intro

--- src/main/java/GetData.java
+++ src/main/java/GetData.java
@@ -3,8 +3,11 @@ import ai.greycat.*;
 public final class GetData {
 
     public static void main(String... args) throws Exception {
-        GreyCat greycat = new GreyCat("http://localhost:8080");
-        std.core.Array<?> data = (std.core.Array<?>) GreyCat.call(greycat, "project::getData");
+
+        String url = "http://localhost:8080";
+
+        GreyCat greycat = new GreyCat(url, null, null, null);
+        std.core.Array<?> data = (std.core.Array<?>) greycat.call("project::getData");
         System.out.println("// Data:" + data);
         System.out.println("// Type: " + data.getClass() + "<" + data.iterator().next().getClass() + ">");
     }
